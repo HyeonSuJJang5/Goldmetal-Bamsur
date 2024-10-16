8a6a03ed8dcddf39fc8d800b3617aa0388611db4
HyeonSuJJang5 <luckysanata@gmail.com>
2024-10-16 13:17:55 +0900

    goldmetal undead survival
    -ing

---
+++ Assets/Undead Survivor/Scripts/Enemy.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float health;
+    public float maxHealth;
+
+    public RuntimeAnimatorController[] animatorController;
+    public Rigidbody2D target;
+
+    bool isLive;
+
+    Rigidbody2D rigid;
+    Collider2D coll;
+    SpriteRenderer spriter;
+    Animator animator;
+    WaitForFixedUpdate wait;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        wait = new WaitForFixedUpdate();
+    }
+
+    void FixedUpdate()
+    {
+        if (!GameManager.instance.isLive)
+            return;
+
+        if (!isLive || animator.GetCurrentAnimatorStateInfo(0).IsName("Hit"))
+            return;
+
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+        rigid.velocity = Vector2.zero;
+    }
+
+    void LateUpdate()
+    {
+        if (!GameManager.instance.isLive)
+            return;
+
+        if (!isLive)
+            return;
+
+        spriter.flipX = target.position.x < rigid.position.x;
+    }
+
+    void OnEnable()
+    {
+        target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+        isLive = true;
+        coll.enabled = true;
+        rigid.simulated = true;
+        spriter.sortingOrder = 2;
+        animator.SetBool("Dead", false);
+        health = maxHealth;
+    }
+
+    public void Init(SpawnData data)
+    {
+        animator.runtimeAnimatorController = animatorController[data.spriteType];
+        speed = data.speed;
+        maxHealth = data.health;
+        health = data.health;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Bullet") || !isLive)
+            return;
+
+        health -= collision.GetComponent<Bullet>().damage;
+        StartCoroutine(KnockBack());
+
+        if (health > 0)
+        {
+            animator.SetTrigger("Hit");
+        }
+        else
+        {
+            isLive = false;
+            coll.enabled = false;
+            rigid.simulated = false;
+            spriter.sortingOrder = 1;
+            animator.SetBool("Dead", true);
+            GameManager.instance.kill++;
+            GameManager.instance.GetExp();
+        }
+    }
+
+    IEnumerator KnockBack()
+    {
+        yield return wait; //다음 하나의 물리 프레임 딜레이
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 dirVec = transform.position - playerPos;
+        rigid.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
+    }
+
+    void Dead()
+    {
+        gameObject.SetActive(false);
+    }
+}
