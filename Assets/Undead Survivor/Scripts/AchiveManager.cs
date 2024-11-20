ab58e8ce86a09279878dba33dbdb92c07f14a673
HyeonSuJJang5 <luckysanata@gmail.com>
2024-11-21 03:21:02 +0900

    14+강

---
+++ Assets/Undead Survivor/Scripts/AchiveManager.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchiveManager : MonoBehaviour
+{
+    public GameObject[] lockChar;
+    public GameObject[] unlockChar;
+    public GameObject uiNotice;
+
+    enum Achive { UnlockCorn, UnlockMeal}
+    Achive[] achives;
+    WaitForSecondsRealtime wait;
+
+    void Awake()
+    {
+        achives = (Achive[])Enum.GetValues(typeof(Achive));
+        wait = new WaitForSecondsRealtime(5);
+
+        if(!PlayerPrefs.HasKey("MyData"))
+        {
+            Init();
+        }
+    }
+
+    void Init()
+    {
+        PlayerPrefs.SetInt("MyData", 1);
+
+        foreach(Achive achive in achives)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 0);
+        }
+    }
+
+    void Start()
+    {
+        UnlockChar();
+    }
+
+    void UnlockChar()
+    {
+        for (int index = 0; index < lockChar.Length; index++)
+        {
+            string achiveName = achives[index].ToString();
+            bool isUnlock = PlayerPrefs.GetInt(achiveName) == 1;
+            lockChar[index].SetActive(!isUnlock);
+            unlockChar[index].SetActive(isUnlock);
+        }
+    }
+
+    void LateUpdate()
+    {
+        foreach(Achive achive in achives)
+        {
+            CheckAchive(achive);
+        }
+    }
+
+    void CheckAchive(Achive achive)
+    {
+        bool isAchive = false;
+
+        switch (achive)
+        {
+            case Achive.UnlockCorn:
+                isAchive = GameManager.instance.kill >= 10;
+                break;
+            case Achive.UnlockMeal:
+                isAchive = GameManager.instance.gameTime == GameManager.instance.maxGameTime;
+                break;
+        }
+
+        if(isAchive && PlayerPrefs.GetInt(achive.ToString()) == 0)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 1);
+
+            for(int index = 0; index < uiNotice.transform.childCount; index++)
+            {
+                bool isActive = index == (int)achive;
+                uiNotice.transform.GetChild(index).gameObject.SetActive(isActive);
+            }
+
+            StartCoroutine(NoticeRoutine());
+        }
+    }
+
+    IEnumerator NoticeRoutine()
+    {
+        uiNotice.SetActive(true);
+
+        yield return wait;
+
+        uiNotice.SetActive(false);
+    }
+}
--- Assets/Undead Survivor/Scripts/GameManager.cs
+++ Assets/Undead Survivor/Scripts/GameManager.cs
@@ -39,7 +39,7 @@ public class GameManager : MonoBehaviour
         health = maxHealth;
 
         player.gameObject.SetActive(true);
-        uiLevelUp.Select(playerId % 4);
+        uiLevelUp.Select(playerId % 2);
         Resume();
     }
 
